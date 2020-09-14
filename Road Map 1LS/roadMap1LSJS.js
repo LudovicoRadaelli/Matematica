624fc8d02b22936c8407a50ae5c9a5f13e14ed71
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2020-09-14 09:24:28 +0200

    Update roadMap1LSJS.js

--- Road Map 1LS/roadMap1LSJS.js
+++ Road Map 1LS/roadMap1LSJS.js
@@ -2,15 +2,15 @@ var percheStudiamoiNumeri = false;
 document.getElementById("percheStudiamoINumeri").addEventListener("click", function(){
     if(percheStudiamoiNumeri === false) {
         document.getElementById("matematicaECervello").style.display = "initial";
-        document.getElementById("argomento2").style.display = "initial";
+        /*document.getElementById("argomento2").style.display = "initial";
         document.getElementById("argomento3").style.display = "initial";
-        document.getElementById("argomento4").style.display = "initial";
+        document.getElementById("argomento4").style.display = "initial";*/
         percheStudiamoiNumeri = true;
     } else {
         document.getElementById("matematicaECervello").style.display = "none";
-        document.getElementById("argomento2").style.display = "none";
+        /*document.getElementById("argomento2").style.display = "none";
         document.getElementById("argomento3").style.display = "none";
-        document.getElementById("argomento4").style.display = "none";
+        document.getElementById("argomento4").style.display = "none";*/
         percheStudiamoiNumeri = false;
     }
 });
