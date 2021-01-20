4dae21e6ad22d201697d806d344bb4d32c31298d
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-01-20 16:36:32 +0100

    Add files via upload

---
+++ 1Liceo/criteriTriangoli/criteriTriangoli.js
@@ -0,0 +1,12 @@
+/*
+
+*/
+
+ss1 = w3.slideshow(".c1", 0);
+ss2 = w3.slideshow(".c2", 0);
+ss3 = w3.slideshow(".c3", 0);
+ss4 = w3.slideshow(".c4", 0);
+
+function chiudi() {
+    document.getElementById("istruzioniWrapper").style.display = "none";
+}
