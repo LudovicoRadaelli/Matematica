aea35f8277f3c328d696b0e1d06f44a336c545dc
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-02-01 12:11:04 +0100

    Delete m directory

--- m/primaTappa.js
+++
@@ -1,7 +0,0 @@
-function changeText() {
-    document.body.innerHTML = "<p>Scherzavo.. tanto manco fumi!<br>Anzi, la salute è importante, perciò...... </p><p>Fai l'esercizio del nuotatore!🏊🏊</p><p>I tuoi premurosi trainer Vinz e Fra decideranno quando potrai smettere</p><button onclick='goToLink()'>Avanti!</button>"
-}
-
-function goToLink() {
-    location.href = "https://ludovicoradaelli.github.io/Matematica/m/secondaTappa.html";
-}
