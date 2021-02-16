df675be56007b2cb95da48a7c99ca01eef42677c
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-02-16 13:00:56 +0100

    Add files via upload

---
+++ 1Liceo/scomposizionePolinomi/divisione/polinomioPolinomio.js
@@ -0,0 +1,8 @@
+function display(i) {
+    document.getElementById("video0").style.display = "none";
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "none";
+    document.getElementById("video"+i).style.display = "block";
+
+}
---
+++ 1Liceo/scomposizionePolinomi/scomposizione/tramiteQuadratoDiBinomio/tramiteQuadratoDiBinomio.js
@@ -0,0 +1,11 @@
+function display(i) {
+    //document.getElementById("video0").style.display = "none";
+    document.getElementById("video0").style.display = "none";
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "none";
+    document.getElementById("video4").style.display = "none";
+    document.getElementById("video5").style.display = "none";
+    document.getElementById("video"+i).style.display = "block";
+
+}
