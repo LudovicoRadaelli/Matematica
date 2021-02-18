56a4eff8883c018b8d4c56629b697296de4b51d3
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-02-18 15:33:18 +0100

    Add files via upload

---
+++ 1Liceo/scomposizionePolinomi/divisione/monomioMonomio.js
@@ -0,0 +1,7 @@
+function display(i) {
+    var vid = document.getElementById("video1");
+    if(i===1) {vid.currentTime= 0;}
+    if(i===2) {vid.currentTime= 85;}
+    if(i===3) {vid.currentTime= 162;}
+    if(i===4) {vid.currentTime= 236;}
+}
---
+++ 1Liceo/scomposizionePolinomi/divisione/polinomioMonomio.js
@@ -0,0 +1,5 @@
+function display(i) {
+    var vid = document.getElementById("video1");
+    if(i===1) {vid.currentTime= 0;}
+    if(i===2) {vid.currentTime= 158;}
+}
