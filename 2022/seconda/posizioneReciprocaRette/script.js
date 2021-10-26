57a2fa65820b00df00e4873069afbbb73fbbccb3
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-10-26 07:07:54 +0200

    Add files via upload

---
+++ 2022/seconda/posizioneReciprocaRette/script.js
@@ -0,0 +1,11 @@
+function mostraPassaggi() {
+    document.getElementById("mostraPassaggi").style.display = "none";
+    document.getElementById("passaggi").style.display = "block";
+    document.getElementById("nascondiPassaggi").style.display = "block";
+}
+
+function nascondiPassaggi() {
+    document.getElementById("mostraPassaggi").style.display = "block";
+    document.getElementById("passaggi").style.display = "none";
+    document.getElementById("nascondiPassaggi").style.display = "none";
+}
