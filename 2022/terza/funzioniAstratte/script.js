5b989994c7942e2d23fac00dce6322ff7dd32cc1
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-10-30 20:45:40 +0200

    Delete 2022/terza/funzioniAstratte directory

--- 2022/terza/funzioniAstratte/script.js
+++
@@ -1,4 +0,0 @@
-
-
-/* MathJax.typesetClear([document.getElementsByClassName("calcoliEquazione"), document.getElementsByClassName("nuovaEquazione")]);
-MathJax.typesetPromise([document.getElementsByClassName("calcoliEquazione"), document.getElementsByClassName("nuovaEquazione")]).then(() => {}); */
