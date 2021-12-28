de3b3cb27f399718805fc68003452f65abd09d70
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2021-12-28 15:49:54 +0100

    Update script.js

--- ambrogiosCircleMetronome/script.js
+++ ambrogiosCircleMetronome/script.js
@@ -1,4 +1,10 @@
-let w = 500;
+let w;
+
+if(window.innerWidth >= 500) {
+  w = 500;
+} else {
+  w = window.innerWidth*2;
+}
 let h = w;
 
 let diam = 7/10*w;
@@ -9,10 +15,14 @@ let beatSub = 3;
 let time = 0;
 let bpm = 60;
 
-let actualBeat = 0;
-let nextBeatTime = 60000/bpm;
+//let actualBeat = 0;
+//let nextBeatTime = 60000/bpm;
 
-let actualBeatSub = -beatSub;
+
+let perc = [];
+updatePerc();
+
+let actualBeatSub;
 let nextBeatSubTime = 60000/bpm; //in questo modo non si sentono le suddivisioni prima dell'uno
 
 let playBool = false;
@@ -85,13 +95,7 @@ function draw() {
   for(let i = 0; i < beatSub*beat; i++) {
     stroke(215);
     strokeWeight(1)
-    line(0,0, r*sin(i*2*PI/(beatSub*beat)), r*cos(i*2*PI/(beatSub*beat)))
-
-    /* if(firstTime) {
-      inputSillabe[i] = createDiv('');
-      div.style('font-size', '20px');
-      div.position(r*sin(i*2*PI/(beatSub*beat)), r*cos(i*2*PI/(beatSub*beat)));
-    } */
+    line(0,0, r*sin(perc[i].angle), r*cos(perc[i].angle));
   }
 
 
@@ -107,7 +111,8 @@ function draw() {
     line(0,0, r*sin(i*2*PI/beat), r*cos(i*2*PI/beat))
   }
 
-  
+  //disegna percentuali
+  drawPercentual();
 
 
   //disegna circonferenza
@@ -121,12 +126,12 @@ function draw() {
     noFill();
     stroke(0);
     strokeWeight(1)
-    ellipse(littleCirc[i].cX*sin(i*2*PI/(beat*beatSub)), littleCirc[i].cY*cos(i*2*PI/(beat*beatSub)), 2*littleCirc[i].ray, 2*littleCirc[i].ray);
+    ellipse(littleCirc[i].cX*sin(perc[i].angle), littleCirc[i].cY*cos(perc[i].angle), 2*littleCirc[i].ray, 2*littleCirc[i].ray);
     if(littleCirc[i].active) {
       textAlign(CENTER, CENTER);
       textSize( Math.min(w/(beat*beatSub)/2, w/30)*1.4)
       resetMatrix();
-      text(littleCirc[i].syl, littleCirc[i].cX*sin(i*2*PI/(beat*beatSub)) + width/2, -littleCirc[i].cY*cos(i*2*PI/(beat*beatSub)) + height/2);
+      text(littleCirc[i].syl, littleCirc[i].cX*sin(perc[i].angle) + width/2, -littleCirc[i].cY*cos(perc[i].angle) + height/2);
       translate(width/2,height/2);
       applyMatrix(1,0,0,-1,0,0);
     }  
@@ -139,13 +144,30 @@ function draw() {
     if(time > nextBeatSubTime) {  
 
       actualBeatSub++;
-      nextBeatSubTime = time + 60000/bpm/beatSub;
+
       console.log(`
-      time: ${time}, 
+      
+      perc index: ${(beatSub*beat + actualBeatSub)%(beatSub*beat)},
+      percentual[${(beatSub*beat + actualBeatSub)%(beatSub*beat)}] = ${perc[(beatSub*beat + actualBeatSub)%(beatSub*beat)].percentual}
       actualBeatSub: ${actualBeatSub}, 
-      nextBeatSubTime: ${nextBeatSubTime}
-      actualBeatSub%(beat*beatSub): ${actualBeatSub%(beat*beatSub)},
-      actualBeatSub%beatSub: ${actualBeatSub%beatSub}`)
+      `)
+
+      if(actualBeatSub < 0) {
+        console.log("out beat");
+        nextBeatSubTime = time + perc[beat*beatSub + actualBeatSub].deltaTime;  
+      } else {
+        nextBeatSubTime = time + perc[actualBeatSub%(beat*beatSub)].deltaTime;
+      }
+      
+      
+
+      /* console.log(`
+      time: ${time}, 
+      delta: ${nextBeatSubTime - time},
+      perc index: ${(beatSub*beat - actualBeatSub)%(beatSub*beat)},
+      percentual[${(beatSub*beat - actualBeatSub)%(beatSub*beat)}] = ${perc[(beatSub*beat + actualBeatSub)%(beatSub*beat)].percentual}
+      actualBeatSub: ${actualBeatSub}, 
+      `) */
 
       
       if(actualBeatSub%(beat*beatSub) === 0) {
@@ -164,18 +186,30 @@ function draw() {
     }
 
 
+    //disegna raggio subBeat
+    stroke(255, 182, 182);
+    strokeWeight(3);
+    line(0,0, r*sin(perc[(beatSub*beat + actualBeatSub)%(beatSub*beat)].angle), r*cos(perc[(beatSub*beat + actualBeatSub)%(beatSub*beat)].angle));
+    
+    strokeWeight(15)
+    point(r*sin(perc[(beatSub*beat + actualBeatSub)%(beatSub*beat)].angle), r*cos(perc[(beatSub*beat + actualBeatSub)%(beatSub*beat)].angle))
+
+
     //disegna raggio
     stroke("red");
     strokeWeight(5);
     line(0,0, r*sin(actualBeat*2*PI/beat), r*cos(actualBeat*2*PI/beat));
     
     strokeWeight(15)
-    point(r*sin(actualBeat*2*PI/beat), r*cos(actualBeat*2*PI/beat))
+    point(r*sin(actualBeat*2*PI/beat), r*cos(actualBeat*2*PI/beat));
 
   } else {
     stroke("red")
     line(0,0, r*sin(-2*PI/beat), r*cos(-2*PI/beat));
   }
+
+
+  
   
 }
 
@@ -186,57 +220,96 @@ function mouseClicked() {
   let mX = mouseX - width/2;
   let mY = -mouseY + height/2;
   for(let i = 0; i < beat*beatSub; i++) {
-    let distMouseCentro = sqrt((mX - littleCirc[i].cX*sin(i*2*PI/(beat*beatSub)))*(mX - littleCirc[i].cX*sin(i*2*PI/(beat*beatSub))) + (mY - littleCirc[i].cY*cos(i*2*PI/(beat*beatSub)))*(mY - littleCirc[i].cY*cos(i*2*PI/(beat*beatSub))))
+    let distMouseCentro = sqrt((mX - littleCirc[i].cX*sin(perc[i].angle))*(mX - littleCirc[i].cX*sin(perc[i].angle)) + (mY - littleCirc[i].cY*cos(perc[i].angle))*(mY - littleCirc[i].cY*cos(perc[i].angle)))
     if( distMouseCentro <= littleCirc[i].ray) {
       littleCirc[i].active = !littleCirc[i].active;
     }
   }
 }
 
+
+//**********************TODO**********************************
+function mousePressed() {
+  let mX = mouseX - width/2;
+  let mY = -mouseY + height/2;
+  let distMouseCentro = sqrt((mX*mX + mY*mY))
+  let angoloMouse;
+  if(mX >= 0) {
+    angoloMouse = PI/2 - atan(mY/mX);
+  } else {
+    angoloMouse = PI + PI/2 - atan(mY/mX);
+  }
+
+  
+
+  for(let i = 0; i < beat*beatSub; i++) {
+    if(distMouseCentro < r && abs(angoloMouse - perc[i].angle) < 0.025  && i%beatSub !==0) {
+      perc[i].grabbed = true;
+      console.log(`
+      distMouseCentro = ${distMouseCentro},
+      angoloMouse = ${angoloMouse*180/PI}
+      `)
+    }
+  }
+}
+
+function mouseDragged() {
+  
+  let mX = mouseX - width/2;
+  let mY = -mouseY + height/2;
+  let angoloMouse;
+  if(mX >= 0) {
+    angoloMouse = PI/2 - atan(mY/mX);
+  } else {
+    angoloMouse = PI + PI/2 - atan(mY/mX);
+  }
+
+
+  for(let i = 1; i < beatSub*beat; i++) {
+    if(perc[i].grabbed && angoloMouse > perc[(i-1)%(beat*beatSub)].angle && angoloMouse < perc[(i+1)%(beat*beatSub)].angle) {
+      
+      for(let j = 0; j < beat; j++) {
+        perc[i + j*beatSub].angle = angoloMouse + j*2*PI/beat;
+      }
+      
+      for(let j = 0; j < beatSub*beat; j++) {
+        if(j === beatSub*beat -1) {
+          perc[j].percentual = perc[j - beatSub].percentual 
+        } else {
+          perc[j].percentual = abs(perc[j].angle - perc[(j+1)%(beat*beatSub)].angle)/(2*PI/beat);
+        }
+
+        
+        perc[j].deltaTime = (60000/bpm)*perc[j].percentual;
+        console.log(`
+        perc[${j}].angle = ${perc[j].angle},
+        perc[${j}].percentual = ${perc[j].percentual}
+        `)
+      }
+    }
+
+    for(let j = 0; j < beatSub; j++) {
+      document.getElementById(`beatSubPercentual${j}`).value = floor(perc[j].percentual*10000)/100;
+    }
+    
+  }
+
+}
+
+function mouseReleased() {
+  for(let i=0; i<beat*beatSub; i++) {
+    perc[i].grabbed = false;
+  }
+}
+
+//************************************************************
+
+
+
 function selectBeat() {
   let option = document.getElementById("beat").options;
   let i = document.getElementById("beat").selectedIndex;
   beat = option[i].index + 2;
-  for(let i = 0; i < beat*beatSub; i++) {
-    littleCirc[i] = {
-      cX: Math.min(r + w/(beat*beatSub), r + w/15),
-      cY: Math.min(r + w/(beat*beatSub), r +w/15),
-      ray: Math.min(w/(beat*beatSub)/2, w/30),
-      active: false
-    }
-
-    if(Math.floor(i/beatSub) === 0) {
-      littleCirc[i].syl = "ti"
-    }
-  
-    if(Math.floor(i/beatSub) === 1) {
-      littleCirc[i].syl = "mo"
-    }
-  
-    if(Math.floor(i/beatSub) === 2) {
-      littleCirc[i].syl = "te"
-    }
-  
-    if(Math.floor(i/beatSub) === 3) {
-      littleCirc[i].syl = "ta"
-    }
-  
-    console.log(`
-    --------------
-    littleCircle${i},
-    cX: ${littleCirc[i].cX},
-    cY: ${littleCirc[i].cY},
-    ray: ${littleCirc[i].ray},
-    beat: ${beat},
-    beatSub: ${beatSub}`)
-  }
-}
-
-
-function selectBeatSub() {
-  let option = document.getElementById("beatSub").options;
-  let i = document.getElementById("beatSub").selectedIndex;
-  beatSub = option[i].index + 1;
   for(let i = 0; i < beat*beatSub; i++) {
     littleCirc[i] = {
       cX: Math.min(r + w/(beat*beatSub), r + w/15),
@@ -270,28 +343,212 @@ function selectBeatSub() {
     beat: ${beat},
     beatSub: ${beatSub}`)
   }
+
+  updatePerc();
+  
+}
+
+
+function selectBeatSub() {
+  let option = document.getElementById("beatSub").options;
+  let i = document.getElementById("beatSub").selectedIndex;
+  beatSub = option[i].index + 1;
+  for(let i = 0; i < beat*beatSub; i++) {
+    littleCirc[i] = {
+      cX: Math.min(r + w/(beat*beatSub), r + w/15),
+      cY: Math.min(r + w/(beat*beatSub), r +w/15),
+      ray: Math.min(w/(beat*beatSub)/2, w/30),
+      active: false
+    }
+
+    if(Math.floor(i/beatSub) === 0) {
+      littleCirc[i].syl = "ti"
+    }
+  
+    if(Math.floor(i/beatSub) === 1) {
+      littleCirc[i].syl = "mo"
+    }
+  
+    if(Math.floor(i/beatSub) === 2) {
+      littleCirc[i].syl = "te"
+    }
+  
+    if(Math.floor(i/beatSub) === 3) {
+      littleCirc[i].syl = "ta"
+    }
+  
+    console.log(`
+    --------------
+    littleCircle${i},
+    cX: ${littleCirc[i].cX},
+    cY: ${littleCirc[i].cY},
+    ray: ${littleCirc[i].ray},
+    beat: ${beat},
+    beatSub: ${beatSub}`)
+  }
+
+  updatePerc();
+
+  for(let i = 0; i < 16; i++) {
+    console.log(`${i}`);
+    document.getElementById(`beatSubPercentual${i}`).style.display = "none";
+  }
+
+  for(let i = 0; i < beatSub; i++) {
+    document.getElementById(`beatSubPercentual${i}`).style.display = "block";
+    document.getElementById(`beatSubPercentual${i}`).style.width = `${Math.round(perc[i].percentual*10000)/100}%`;
+    document.getElementById(`beatSubPercentual${i}`).value = `${Math.round(perc[i].percentual*10000)/100}`;
+    
+
+  }
+  
 }
 
 
 function changeBpmButton(i) {
   bpm = parseInt(bpm) + i;
   document.getElementById("bpm").innerHTML = `${bpm}`;
-  document.getElementById("bpm-slider").value = bpm
+  document.getElementById("bpm-slider").value = bpm;
+  updatePerc();
 }
 
 function  changeBpmSlider() {
   bpm = document.getElementById("bpm-slider").value;
   document.getElementById("bpm").innerHTML = `${bpm}`;
+  updatePerc();
 }
 
 function  play(bool) {
-
-  /* if(!bool) {
-    firstTime = true;
-  } */
   
   playBool = bool;
-  actualBeat = -1;
-  actualBeatSub = -2;
+  actualBeat = -2;
+  actualBeatSub = -beatSub - 1;
+
+  console.clear();
+  console.log(`
+  ${actualBeat},
+  ${actualBeatSub}`)
   
 }
+
+function updatePerc() {
+  for(let i = 0; i < beatSub*beat; i++) {
+    if(i%beatSub === 0) {
+      perc[i] = {
+        percentual: 1/beatSub,
+        angle: i/beatSub*2*Math.PI/beat,
+        grabbed: false
+      };
+      perc[i].deltaTime = (60000/bpm)*perc[i].percentual;
+    } else if(i%beatSub === beatSub - 1) {
+        let percentualSum = 0;
+        let angleSum = 0;
+          for(let j = 0; j < beatSub -1; j++) {
+            percentualSum += perc[j].percentual;
+            angleSum += perc[j].angle;
+          }
+        perc[i] = {
+          percentual: 1 - percentualSum,
+          angle: perc[i-1].angle + perc[i-1].percentual*2*Math.PI/beat,
+          grabbed: false
+        };
+        
+        //
+        perc[i].deltaTime = (60000/bpm)*perc[i].percentual;
+    } else {
+        perc[i] = {
+          percentual: 1/beatSub,
+          angle: perc[i-1].angle + perc[i-1].percentual*2*Math.PI/beat,
+          grabbed: false
+        };
+        perc[i].deltaTime = (60000/bpm)*perc[i].percentual;
+    }
+  
+    
+    console.log(`
+    angolo[${i}] = ${perc[i].angle/Math.PI*180},
+    percentuale[${i}] = ${perc[i].percentual},
+    deltaTime[${i}] = ${perc[i].deltaTime}
+    `)
+  }
+}
+
+function changePercentual() {
+  
+  let percInput = [];
+  for(let i = 0; i < beatSub; i++) {
+    percInput[i] = document.getElementById(`beatSubPercentual${i}`).value/100;
+  }
+
+  for(let i = 0; i < beatSub*beat; i++) {
+    if(i%beatSub === 0) {
+      perc[i] = {
+        percentual: percInput[i%beatSub],
+        angle: i/beatSub*2*Math.PI/beat,
+      };
+      perc[i].deltaTime = (60000/bpm)*perc[i].percentual;
+    } else if(i%beatSub === beatSub - 1) {
+        let percentualSum = 0;
+        let angleSum = 0;
+          for(let j = 0; j < beatSub -1; j++) {
+            percentualSum += perc[j].percentual;
+            angleSum += perc[j].angle;
+          }
+        perc[i] = {
+          percentual: 1 - percentualSum,
+          angle: perc[i-1].angle + perc[i-1].percentual*2*Math.PI/beat,
+        };
+        
+        perc[i].deltaTime = (60000/bpm)*perc[i].percentual;
+    } else {
+        perc[i] = {
+          percentual: percInput[i%beatSub],
+          angle: perc[i-1].angle + perc[i-1].percentual*2*Math.PI/beat,
+        };
+        perc[i].deltaTime = (60000/bpm)*perc[i].percentual;
+    }
+  
+  
+    
+    console.log(`
+    angolo[${i}] = ${perc[i].angle/Math.PI*180},
+    percentuale[${i}] = ${perc[i].percentual},
+    deltaTime[${i}] = ${perc[i].deltaTime}
+    `)
+  }
+}
+
+function drawPercentual() {
+  if(document.getElementById("changeAll").checked) {
+    push();
+
+    resetMatrix();
+    translate(width/2,height/2);
+    rotate(-PI/2)
+    
+    //per impostare la textSize vediamo quale è l'angolo più piccolo tra un raggio e l'altro
+    let indexMin = 0;
+    for(let i = 1; i < beatSub; i++) {
+      if(perc[i].percentual < perc[i-1].percentual) {
+        indexMin = i;
+      }
+    }
+    textSize(cos(Math.abs(perc[(indexMin + 1)].angle - perc[indexMin].angle))/2*25);
+    
+    
+    for(let i = 0; i < beatSub; i++) {
+      textAlign(CENTER, CENTER);
+      let textAngle = Math.abs(perc[(i + 1)%(beat*beatSub)].angle - perc[i%(beat*beatSub)].angle)/2;
+      noFill();
+      stroke(0);
+      strokeWeight(1);
+      
+      rotate(textAngle);      
+      text(`${Math.round(perc[i].percentual*10000)/100}%`, r/2, 0);
+      rotate(textAngle);
+
+    }
+    pop();
+  }
+}
+
