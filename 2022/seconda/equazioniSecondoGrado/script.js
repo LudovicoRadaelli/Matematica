82480849795d58a59c9aae0fe6dcd184a6264dd3
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2022-03-23 17:27:30 +0100

    Update script.js

--- 2022/seconda/equazioniSecondoGrado/script.js
+++ 2022/seconda/equazioniSecondoGrado/script.js
@@ -168,7 +168,51 @@ function generaEsempio(n) {
 
     }
 
-    
     MathJax.typesetClear([esempio]);
     MathJax.typesetPromise([esempio]).then(() => {});
 }
+
+
+ /* ------------------------------------------------------------------------- */
+
+
+
+    var ggbApp1 = new GGBApplet({
+        "appName": "classic", 
+        "id": "gg1",
+        "showToolBar": false, 
+        "showAlgebraInput": false, 
+        "showMenuBar": false, 
+        "enableShiftDragZoom": false,
+        "scaleContainerClass":"geogebra-container1",
+        "material_id":"pmtwysdq"}, true);
+
+    var ggbApp2 = new GGBApplet({
+        "appName": "classic", 
+        "id": "gg2",
+        "showToolBar": false, 
+        "showAlgebraInput": false, 
+        "showMenuBar": false, 
+        "enableShiftDragZoom": false,
+        "scaleContainerClass":"geogebra-container2",
+        "material_id":"gtc37bt3"}, true);
+    
+    window.addEventListener("load", function() { 
+        ggbApp1.inject('geogebra1');
+        ggbApp2.inject('geogebra2');
+    });
+
+    function disegnaPunto() {
+        if(document.getElementById("coordX").value === `` || document.getElementById("coordY").value === `` || document.getElementById("coordY").value != document.getElementById("coordX").value -1) {
+            alert(`input errati`);
+        } else {
+            let x = document.getElementById("coordX").value;
+            let y = document.getElementById("coordY").value;
+            gg1.evalCommand(`(${x}, ${y})`);
+        }
+
+    }
+
+    function disegnaPolinomio() {
+        gg1.evalCommand(`y = x -1`);
+    }
